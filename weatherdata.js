5816975aa957acbda869adbd1996964dbada3732
Eimen Kiose <161055517+xek1923x@users.noreply.github.com>
2025-10-19 16:23:21 +0000

    Very far along

--- weatherdata.js
+++ weatherdata.js
@@ -19,12 +19,61 @@ async function getCurrentWeather(lat, lon) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        
+
         document.getElementById("wthr").innerText = `${Math.round(data.current.temperature_2m)}°C`;
         document.getElementById("feel").innerText = `feels like: ${Math.round(data.current.apparent_temperature)}°C`;
+        const currentHour = data.current.time.split("T")[1].split(":")[0];
+        document.getElementById("wdescription").innerText = `${currentHour}:00`;
+        
+        getHourlyWeather(lat, lon, currentHour)
+
     } catch (error) {
         console.error("Error fetching weather data:", error);
     }
 }
 
 getLocation();
+
+async function getHourlyWeather(lat, lon, currentHour) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,weather_code&timezone=auto&forecast_days=3`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        renderHourly(data.hourly, currentHour);
+    } catch (error) {
+        console.error("Error fetching hourly weather data:", error);
+    }
+    
+}
+
+async function renderHourly(hourlyData, currentHour) {
+    const hourlyContainer = document.getElementById("hourlycards");
+    hourlyContainer.innerHTML = "";
+
+    let cur = 0;
+    for (let a = 0; a < hourlyData.time.length; a++) {
+        let dhour = parseInt(hourlyData.time[a].split("T")[1].split(":")[0]);
+        if (dhour == currentHour) {
+            cur = a;
+            break;
+        }
+    }
+    for (let i = cur; i < cur + 24; i++) {
+        let hour = parseInt(hourlyData.time[i].split("T")[1].split(":")[0]);
+        const card = document.createElement("div");
+        card.className = "hourlycard";
+
+        const time = document.createElement("div");
+        time.className = "hourlytime";
+        time.innerText = `${hour}:00`;
+
+        const temp = document.createElement("div");
+        temp.className = "hourlytemp";
+        temp.innerText = `${Math.round(hourlyData.temperature_2m[i])}°C`;
+
+        card.appendChild(time);
+        card.appendChild(temp);
+        hourlyContainer.appendChild(card);
+    }
+}
